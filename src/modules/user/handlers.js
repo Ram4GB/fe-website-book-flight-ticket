6e971695ed2a1bea8dc328823fab367a344a3e5e
Duc Thuan <dotthan123@gmail.com>
2019-09-22 17:54:29 +0700

    fix token

--- src/modules/user/handlers.js
+++ src/modules/user/handlers.js
@@ -1,41 +1,41 @@
-import * as actions from './actions'
-export default function (dispatch, props) {
+import * as actions from "./actions";
+export default function(dispatch, props) {
   return {
     getProfileUser: () => {
-      const user = window.localStorage.getItem('user')
-      const { token } = JSON.parse(user)
-      let result = {}
-      if (token) {
-        // fetch user login again and dispatch to redux again
-        result = {
-          id: 1,
-          username: 'leminhcuong298@yahoo.com.vn',
-          password: '123456789',
-          role: 'admin',
-          token: 'qwertyuiop'
-        }
-        dispatch(actions.login(result))
-      } else console.log('User must be login')
+      const user = window.localStorage.getItem("user");
+      if (user) {
+        const { token } = JSON.parse(user);
+        let result = {};
+        if (token) {
+          // fetch user login again and dispatch to redux again
+          result = {
+            id: 1,
+            username: "leminhcuong298@yahoo.com.vn",
+            password: "123456789",
+            role: "admin",
+            token: "qwertyuiop"
+          };
+          dispatch(actions.login(result));
+        } else console.log("User must be login");
 
-      dispatch(actions.login(result))
+        dispatch(actions.login(result));
+      }
     },
     login: () => {
       const result = {
         id: 1,
-        username: 'leminhcuong298@yahoo.com.vn',
-        password: '123456789',
-        role: 'admin',
-        token: 'qwertyuiop'
-      }
-      dispatch(actions.login(result))
-      window.localStorage.setItem('user', JSON.stringify(result))
+        username: "leminhcuong298@yahoo.com.vn",
+        password: "123456789",
+        role: "admin",
+        token: "qwertyuiop"
+      };
+      dispatch(actions.login(result));
+      window.localStorage.setItem("user", JSON.stringify(result));
     },
     logout: () => {
-      dispatch(actions.logout())
-      window.localStorage.removeItem('user')
+      dispatch(actions.logout());
+      window.localStorage.removeItem("user");
     },
-    getData: () => {
-
-    }
-  }
+    getData: () => {}
+  };
 }
