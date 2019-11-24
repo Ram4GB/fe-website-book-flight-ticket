13969f06ae01c4e31991693a3bc96f4a0fd918e6
Ram4GB <leminhcuong2988@yahoo.com.vn>
2019-11-24 16:35:48 +0700

    api for dashboard

--- src/modules/user/handlers.js
+++ src/modules/user/handlers.js
@@ -1,160 +1,187 @@
-import * as actions from './actions'
-import Cookies from '../../../node_modules/js-cookie/src/js.cookie'
-import { DEFAULT_URL } from '../../common/url'
-import { fetchLoading, fetchAuthLoading } from '../../common/effects'
+import * as actions from "./actions";
+import Cookies from "../../../node_modules/js-cookie/src/js.cookie";
+import { DEFAULT_URL } from "../../common/url";
+import { fetchLoading, fetchAuthLoading } from "../../common/effects";
+
 export const getListStaffAsync = async (page, params) => {
   const result = await fetchAuthLoading({
     url: `${DEFAULT_URL}/staff`,
-    method: 'GET',
+    method: "GET",
     params: {
       ...params,
-      page,
-    },
-  })
-  return result
-}
+      page
+    }
+  });
+  return result;
+};
 export const getListCustomerAsync = async (page, params) => {
   const result = await fetchAuthLoading({
     url: `${DEFAULT_URL}/customer`,
-    method: 'GET',
+    method: "GET",
     params: {
       ...params,
-      page,
-    },
-  })
-  return result
-}
+      page
+    }
+  });
+  return result;
+};
 export const getCustomerByID = async id => {
   let result = await fetchAuthLoading({
     url: `${DEFAULT_URL}/customer/${id}`,
-    method: 'GET',
-  })
-  return result.data
-}
+    method: "GET"
+  });
+  return result.data;
+};
 export const updateCustomerByID = async (data, id) => {
   let result = await fetchAuthLoading({
     url: `${DEFAULT_URL}/customer/${id}`,
-    method: 'PUT',
-    data,
-  })
-  return result.data
-}
+    method: "PUT",
+    data
+  });
+  return result.data;
+};
 export const getStaffByID = async id => {
   let result = await fetchAuthLoading({
     url: `${DEFAULT_URL}/staff/${id}`,
-    method: 'GET',
-  })
-  return result.data
-}
+    method: "GET"
+  });
+  return result.data;
+};
 export const updateStaffByID = async (data, id) => {
   let result = await fetchAuthLoading({
     url: `${DEFAULT_URL}/staff/${id}`,
-    method: 'PUT',
-    data,
-  })
-  return result.data
-}
+    method: "PUT",
+    data
+  });
+  return result.data;
+};
 export const addStaffAsync = async data => {
   let result = await fetchAuthLoading({
     url: `${DEFAULT_URL}/staff`,
     data,
-    method: 'POST',
-  })
-  return result
-}
+    method: "POST"
+  });
+  return result;
+};
 export const addCustomerAsync = async data => {
   let result = await fetchAuthLoading({
     url: `${DEFAULT_URL}/customer`,
     data,
-    method: 'POST',
-  })
-  return result
-}
+    method: "POST"
+  });
+  return result;
+};
 export default function(dispatch, props) {
   return {
     getProfileUser: async () => {
-      const token = Cookies.get('token')
+      const token = Cookies.get("token");
       if (token) {
         // fetch user login again and dispatch to redux again
         let result = await fetchAuthLoading({
           url: `${DEFAULT_URL}/auth/me`,
-          method: 'GET',
-        })
+          method: "GET"
+        });
         // console.log(result);
         if (result && result.data && result.data.success === true) {
-          dispatch(actions.getMe(result.data.data))
+          dispatch(actions.getMe(result.data.data));
         }
-      } else dispatch(actions.getMe({}))
+      } else dispatch(actions.getMe({}));
     },
     login: async (email, password) => {
       const result = await fetchLoading({
         url: `${DEFAULT_URL}/auth/login`,
         data: {
           email,
-          password,
+          password
         },
-        method: 'POST',
-      })
+        method: "POST"
+      });
       if (result && result.data) {
-        const { data, exp, token } = result.data
+        const { data, exp, token } = result.data;
         if (data) {
           // isUserExist === true
-          dispatch(actions.login(data))
-          Cookies.set('user', data)
-          Cookies.set('token', token)
-          Cookies.set('exp', exp)
-          return result.data
-        } else return result.data
+          dispatch(actions.login(data));
+          Cookies.set("user", data);
+          Cookies.set("token", token);
+          Cookies.set("exp", exp);
+          return result.data;
+        } else return result.data;
       }
-      return { success: false, error: 'Server error' }
+      return { success: false, error: "Server error" };
     },
     logout: () => {
-      dispatch(actions.logout())
-      Cookies.remove('user', { path: '/' })
-      Cookies.remove('token', { path: '/' })
-      Cookies.remove('exp', { path: '/' })
-      localStorage.clear()
+      dispatch(actions.logout());
+      Cookies.remove("user", { path: "/" });
+      Cookies.remove("token", { path: "/" });
+      Cookies.remove("exp", { path: "/" });
+      localStorage.clear();
       setTimeout(() => {
-        window.location.href = '/'
-      }, 1000)
+        window.location.href = "/";
+      }, 1000);
     },
     getListStaff: async (page, params) => {
-      const result = await getListStaffAsync(page, params)
+      const result = await getListStaffAsync(page, params);
       if (result && result.data) {
-        dispatch(actions.getListStaff(result.data.data)) // dispatch list user
-        return result.data
+        dispatch(actions.getListStaff(result.data.data)); // dispatch list user
+        return result.data;
       }
     },
     getListCustomer: async (page, params) => {
-      const result = await getListCustomerAsync(page, params)
+      const result = await getListCustomerAsync(page, params);
       // console.log(result);
       if (result && result.data) {
-        dispatch(actions.getListUser(result.data.data)) // dispatch list user
-        return result.data
+        dispatch(actions.getListUser(result.data.data)); // dispatch list user
+        return result.data;
       }
     },
     addStaff: async data => {
-      let result = await addStaffAsync(data)
+      let result = await addStaffAsync(data);
       if (result && result.data) {
-        return result.data
-      } else return { success: false, error: 'Server error' }
+        return result.data;
+      } else return { success: false, error: "Server error" };
     },
     addCustomer: async data => {
-      let result = await addCustomerAsync(data)
-      console.log(result)
+      let result = await addCustomerAsync(data);
+      console.log(result);
       if (result && result.data) {
-        return result.data
-      } else return { success: false, error: 'Server error' }
+        return result.data;
+      } else return { success: false, error: "Server error" };
     },
     signUp: async data => {
       let result = await fetchLoading({
         url: `${DEFAULT_URL}/customer/signup`,
         data,
-        method: 'POST',
-      })
+        method: "POST"
+      });
       if (result && result.data) {
-        return result.data
-      } else return { success: false, error: 'Server error' }
+        return result.data;
+      } else return { success: false, error: "Server error" };
     },
-  }
+    generalStatic: async (year, month) => {
+      let params;
+      if (year) params.year = year;
+      if (month) params.month = month;
+      let result = await fetchAuthLoading({
+        url: `${DEFAULT_URL}/statistic/general/`,
+        method: "GET",
+        params: {
+          ...params
+        }
+      });
+      if (result && result.data) return result.data;
+      return { success: false, error: "Server error" };
+    },
+    yearStatic: async year => {
+      let params;
+      if (year) params.year = year;
+      let result = await fetchAuthLoading({
+        url: `${DEFAULT_URL}/statistic/year`,
+        params: {
+          ...params
+        }
+      });
+      if (result && result.data) return result.data;
+      return { success: false, error: "Server error" };
+    }
+  };
 }
--- src/pages/DashboardAdmin.js
+++ src/pages/DashboardAdmin.js
@@ -1,257 +1,270 @@
-import React, { Component } from 'react'
-import { Card, Icon, Table } from 'antd'
-import { Chart, Geom, Axis, Tooltip } from 'bizcharts'
-import Column from 'antd/lib/table/Column'
-const data2 = [
-  {
-    year: '1991',
-    value: 3,
-  },
-  {
-    year: '1992',
-    value: 4,
-  },
-  {
-    year: '1993',
-    value: 3.5,
-  },
-  {
-    year: '1994',
-    value: 5,
-  },
-  {
-    year: '1995',
-    value: 4.9,
-  },
-  {
-    year: '1996',
-    value: 6,
-  },
-  {
-    year: '1997',
-    value: 7,
-  },
-  {
-    year: '1998',
-    value: 9,
-  },
-  {
-    year: '1999',
-    value: 13,
-  },
-]
+import React, { Component } from "react";
+import { Card, Icon, Table } from "antd";
+import { Chart, Geom, Axis, Tooltip, Legend } from "bizcharts";
+import Column from "antd/lib/table/Column";
+
+import { connect } from "react-redux";
+import handlers from "../modules/user/handlers";
+import numeral from "numeral";
+
 const cols2 = {
-  value: {
+  revenue: {
     min: 0,
+    formatter: value => {
+      return numeral(value).format("0,0");
+    }
   },
-  year: {
-    range: [0, 1],
-  },
-}
+  month: {
+    range: [0, 1]
+  }
+};
 const cols = {
   sales: {
-    tickInterval: 20,
-  },
-}
+    tickInterval: 20
+  }
+};
 const data = [
   {
-    year: '1951 年',
-    sales: 38,
+    year: "1951 年",
+    sales: 38
   },
   {
-    year: '1952 年',
-    sales: 52,
+    year: "1952 年",
+    sales: 52
   },
   {
-    year: '1956 年',
-    sales: 61,
+    year: "1956 年",
+    sales: 61
   },
   {
-    year: '1957 年',
-    sales: 145,
+    year: "1957 年",
+    sales: 145
   },
   {
-    year: '1958 年',
-    sales: 48,
+    year: "1958 年",
+    sales: 48
   },
   {
-    year: '1959 年',
-    sales: 38,
+    year: "1959 年",
+    sales: 38
   },
   {
-    year: '1960 年',
-    sales: 38,
+    year: "1960 年",
+    sales: 38
   },
   {
-    year: '1962 年',
-    sales: 38,
-  },
-]
+    year: "1962 年",
+    sales: 38
+  }
+];
 export class DashboardAdmin extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      generalStatic: null,
+      yearStatic: []
+    };
+    this.getGeneralStatics = this.getGeneralStatics.bind(this);
+  }
+  async getGeneralStatics() {
+    let result = await this.props.generalStatic();
+    if (result && result.success) {
+      this.setState({
+        generalStatic: result
+      });
+    }
+  }
+  async yearStatic() {
+    let result = await this.props.yearStatic();
+    if (result && result.success) {
+      this.setState({
+        yearStatic: result.data
+      });
+    }
+  }
   componentWillMount() {
     document.title = "FlyNow | Bảng điều khiển";
   }
+  componentDidMount() {
+    this.getGeneralStatics();
+    this.yearStatic();
+  }
   render() {
+    const { generalStatic, yearStatic } = this.state;
+    console.log(yearStatic);
     return (
       <div>
-        <div className='d-flex row-custom'>
-          <div className='col-card-3'>
-            <Card className='card-1'>
-              <div className='d-flex justify-content-between '>
-                <span className='card-header-custom'>Khách hàng</span>
-                <div className='icon-wrap'>
-                  <i className='fal fa-users icon-custom'></i>
-                </div>
+        <div className="d-flex row-custom">
+          {generalStatic ? (
+            <>
+              <div className="col-card-3">
+                <Card className="card-1">
+                  <div className="d-flex justify-content-between ">
+                    <span className="card-header-custom">Khách hàng</span>
+                    <div className="icon-wrap">
+                      <i className="fal fa-users icon-custom"></i>
+                    </div>
+                  </div>
+                  <div className="number">{generalStatic.customer.total} </div>
+                  <div>
+                    <Icon className="custom-icon" type="arrow-up" />
+                    <span className="percent" style={{ color: "green" }}>
+                      {generalStatic.customer.percent.toFixed(2)}%
+                    </span>
+                    <span style={{ color: "#6a6a82 !important" }}>
+                      Trong tháng qua
+                    </span>
+                  </div>
+                </Card>
               </div>
-              <div className='number'>32,000</div>
-              <div>
-                <Icon className='custom-icon' type='arrow-up' />
-                <span className='percent' style={{ color: 'green' }}>
-                  5.15%{' '}
-                </span>
-                <span style={{ color: '#6a6a82 !important' }}>
-                  Since last month
-                </span>
+              <div className="col-card-3">
+                <Card className="card-1">
+                  <div className="d-flex justify-content-between ">
+                    <span className="card-header-custom">Đối tác</span>
+                    <div className="icon-wrap">
+                      <i className="fal fa-handshake icon-custom"></i>
+                    </div>
+                  </div>
+                  <div className="number">{generalStatic.airline.total}</div>
+                  <div>
+                    <Icon className="custom-icon" type="arrow-up" />
+                    <span className="percent" style={{ color: "green" }}>
+                      {generalStatic.airline.percent.toFixed(2)}%{" "}
+                    </span>
+                    <span style={{ color: "#6a6a82 !important" }}>
+                      Trong tháng qua
+                    </span>
+                  </div>
+                </Card>
               </div>
-            </Card>
-          </div>
-          <div className='col-card-3'>
-            <Card className='card-1'>
-              <div className='d-flex justify-content-between '>
-                <span className='card-header-custom'>Đối tác</span>
-                <div className='icon-wrap'>
-                  <i className='fal fa-handshake icon-custom'></i>
-                </div>
+              <div className="col-card-3">
+                <Card className="card-1">
+                  <div className="d-flex justify-content-between ">
+                    <span className="card-header-custom">Doanh thu</span>
+                    <div className="icon-wrap">
+                      <i className="fal fa-money-bill icon-custom"></i>
+                    </div>
+                  </div>
+                  <div className="number">
+                    {numeral(generalStatic.revenue.revenue).format("0,0")}
+                  </div>
+                  <div>
+                    <Icon className="custom-icon" type="arrow-up" />
+                    <span className="percent" style={{ color: "green" }}>
+                      {generalStatic.revenue.percent.toFixed(2)}%{" "}
+                    </span>
+                    <span style={{ color: "#6a6a82 !important" }}>
+                      Trong tháng qua
+                    </span>
+                  </div>
+                </Card>
               </div>
-              <div className='number'>32</div>
-              <div>
-                <Icon className='custom-icon' type='arrow-up' />
-                <span className='percent' style={{ color: 'green' }}>
-                  5.15%{' '}
-                </span>
-                <span style={{ color: '#6a6a82 !important' }}>
-                  Since last month
-                </span>
-              </div>
-            </Card>
-          </div>
-          <div className='col-card-3'>
-            <Card className='card-1'>
-              <div className='d-flex justify-content-between '>
-                <span className='card-header-custom'>Doanh thu</span>
-                <div className='icon-wrap'>
-                  <i className='fal fa-money-bill icon-custom'></i>
-                </div>
-              </div>
-              <div className='number'>32,000,000</div>
-              <div>
-                <Icon className='custom-icon' type='arrow-up' />
-                <span className='percent' style={{ color: 'green' }}>
-                  5.15%{' '}
-                </span>
-                <span style={{ color: '#6a6a82 !important' }}>
-                  Since last month
-                </span>
-              </div>
-            </Card>
-          </div>
+            </>
+          ) : null}
         </div>
         <div
-          className='d-flex justify-content-between'
+          className="d-flex justify-content-between"
           style={{ marginTop: 25 }}
         >
           <Card
-            className='card-1'
-            title={<div className='title'>thống kê doanh thu tháng 11</div>}
-            style={{ width: '49%', height: 620 }}
+            className="card-1"
+            title={<div className="title">thống kê doanh thu tháng 11</div>}
+            style={{ width: "49%", height: 620 }}
           >
             <Table
               pagination={{
-                size: 'small',
-                pageSize: 5,
+                size: "small",
+                pageSize: 5
               }}
               rowKey={e => e.id}
               dataSource={(function() {
-                let a = []
+                let a = [];
                 for (let i = 0; i < 11; i++) {
                   a.push({
                     id: i,
                     1: `MH37${i}`,
-                    2: 'VietName Airline',
+                    2: "VietName Airline",
                     3: 50,
                     4: 40,
-                    5: '80%',
-                    6: '100$',
-                  })
+                    5: "80%",
+                    6: "100$"
+                  });
                 }
-                return a
+                return a;
               })()}
             >
               <Column
-                title='Số hiệu chuyến bay'
-                align='center'
-                dataIndex='1'
+                title="Số hiệu chuyến bay"
+                align="center"
+                dataIndex="1"
               ></Column>
               <Column
-                align='center'
-                title='Hãng hàng không'
-                dataIndex='2'
+                align="center"
+                title="Hãng hàng không"
+                dataIndex="2"
               ></Column>
-              <Column align='center' title='Tổng số vé' dataIndex='3'></Column>
-              <Column align='center' title='Số vé bán' dataIndex='4'></Column>
-              <Column align='center' title='Tỉ lệ' dataIndex='5'></Column>
-              <Column align='center' title='Doanh thu' dataIndex='6'></Column>
+              <Column align="center" title="Tổng số vé" dataIndex="3"></Column>
+              <Column align="center" title="Số vé bán" dataIndex="4"></Column>
+              <Column align="center" title="Tỉ lệ" dataIndex="5"></Column>
+              <Column align="center" title="Doanh thu" dataIndex="6"></Column>
             </Table>
             {/* <div className="title">thống kê doanh thu tháng 11</div> */}
           </Card>
           <Card
-            className='card-1'
-            title={<div className='title'>thống kê theo loại vé</div>}
-            style={{ width: '49%' }}
+            className="card-1"
+            title={<div className="title">thống kê theo loại vé</div>}
+            style={{ width: "49%" }}
           >
             <Chart height={500} data={data} scale={cols} forceFit>
-              <Axis name='year' />
-              <Axis name='sales' />
+              <Axis name="year" />
+              <Axis name="sales" />
               <Tooltip
                 crosshairs={{
-                  type: 'y',
+                  type: "y"
                 }}
               />
-              <Geom type='interval' position='year*sales' />
+              <Geom type="interval" position="year*sales" />
             </Chart>
             {/* <div className="title">thống kê theo loại vé</div> */}
           </Card>
         </div>
         <Card
-          className='card-1'
-          title={<div className='title'>thống kê doanh thu từng tháng</div>}
+          className="card-1"
+          title={<div className="title">thống kê doanh thu từng tháng</div>}
           style={{ marginTop: 25 }}
         >
-          <Chart height={400} data={data2} scale={cols2} forceFit>
-            <Axis name='year' />
-            <Axis name='value' />
+          <Chart height={400} data={yearStatic} scale={cols2} forceFit>
+            <Axis name="month" />
+            <Axis name="revenue" />
             <Tooltip
               crosshairs={{
-                type: 'y',
+                type: "y"
               }}
             />
-            <Geom type='line' position='year*value' size={2} />
+            <Legend />
+            <Geom type="line" position="month*revenue" size={2} />
             <Geom
-              type='point'
-              position='year*value'
+              type="point"
+              position="month*revenue"
               size={4}
-              shape={'circle'}
+              shape={"circle"}
               style={{
-                stroke: '#fff',
-                lineWidth: 1,
+                stroke: "#fff",
+                lineWidth: 1
               }}
             />
           </Chart>
           {/* <div className="title">thống kê doanh thu từng tháng</div> */}
         </Card>
       </div>
-    )
+    );
   }
 }
 
-export default DashboardAdmin
+const mapDispatchToProps = dispatch => {
+  return {
+    ...handlers(dispatch)
+  };
+};
+
+export default connect(null, mapDispatchToProps)(DashboardAdmin);
